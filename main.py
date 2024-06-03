9ea941e7a014245113becb5733826db649d75d5e
Jennie.dob <jennie.dobrovolsky@houzz.com>
2024-06-03 14:30:42 +0300

    used right python this time

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from time import time, sleep
 from api4jenkins import Jenkins as OriginalJenkins
 print("PYTHONPATH:", sys.path)
 print("Command-line arguments:", sys.argv)
+print("Environment variables:")
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
 
 sys.path.append(os.path.abspath('/app/api4jenkins'))
 # print python version
